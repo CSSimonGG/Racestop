7bd8b1568e099d1e0fede73b4a6d60ad63594006
Simon <satmdeklerk@gmail.com>
2023-06-07 19:48:46 +0200

    home page

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//
+Route::get('/home', function () {
+    return view('home');
+});
+
+// User Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// User Profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
