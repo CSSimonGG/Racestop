65346bcb40bf89a2586a5afc2d05351015d95399
Simon <satmdeklerk@gmail.com>
2023-06-07 19:28:47 +0200

    vue js setup done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import "./bootstrap";
 
-import { createApp } from "vue/dist/vue.esm-bundler.js";
+import Example from "./components/example.vue";
+import { createApp } from "vue";
 
-const app = createApp({});
+const app = createApp({
+    components: {
+        Example,
+    },
+});
 
 app.mount("#app");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,9 @@
 </head>
 
 <body>
-    <div id="app"></div>
+    <div id="app">
+        <example />
+    </div>
 </body>
 
 </html>
