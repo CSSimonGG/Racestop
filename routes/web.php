503fae9d8e7bae0ac9d030d66abdf89dea4f5bd6
Simon <satmdeklerk@gmail.com>
2023-06-07 21:08:49 +0200

    removed and fixed some stuff

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Home
 Route::get('/', function () {
-    return view('welcome');
-});
-
-//
-Route::get('/home', function () {
     return view('home');
 });
 
