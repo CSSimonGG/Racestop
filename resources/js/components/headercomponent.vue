76b7a696bad96fb983280576fff73759af67b836
Simon <satmdeklerk@gmail.com>
2023-06-23 01:00:32 +0200

    Header Progress

--- resources/js/components/headercomponent.vue
+++ resources/js/components/headercomponent.vue
@@ -2,7 +2,32 @@
 
 <template>
     <header class="bg-white">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"></div>
+        <div>
+            <div class="max-w-5xl mx-auto flex items-center justify-between">
+                <div class="bg-white py-2 px-4 w-8/12">
+                    <h1 class="text-light-red text-3xl font-extrabold">
+                        RACESTOP
+                    </h1>
+                </div>
+                <span
+                    class="h-[3.25rem] w-4/12 bg-dark-red after:absolute after:w-full after:h-[3.25rem] after:bg-dark-red"
+                ></span>
+            </div>
+        </div>
+        <div class="max-w-5xl mx-auto p-4">
+            <nav class="font-night font-bold">
+                <ul class="flex space-x-12">
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/f1/news">F1 NEWS</a></li>
+                    <li><a href="/f1/calender">F1 CALENDER</a></li>
+                    <li><a href="/f1/statistics">F1 STATISTICS</a></li>
+                    <li><a href="/f1/standings">F1 STANDINGS</a></li>
+                    <li><a href="/">CHAMPIONSHIPS</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="bg-yellow p-0.5"></div>
+        <div class="bg-night p-0.5"></div>
     </header>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,32 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: [
+                    "Open Sans",
+                    "sans-serif",
+                    ...defaultTheme.fontFamily.sans,
+                ],
             },
+        },
+        colors: {
+            yellow: "#FAD732",
+            "dark-red": "#D00000",
+            "light-red": "#ED1A1A",
+            night: "#131315",
+            jet: "#2F2F2F",
+            white: "#FFFFFF",
         },
     },
 
