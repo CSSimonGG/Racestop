4d9c69cdec08d3c782300693530685a9c4acf91e
Simon <satmdeklerk@gmail.com>
2023-07-08 22:13:45 +0200

    Header Fixed and Socials Added

--- resources/js/components/headercomponent.vue
+++ resources/js/components/headercomponent.vue
@@ -371,7 +371,7 @@ export default {
             </nav>
         </div>
         <div
-            :class="showMenu ? 'flex' : 'hidden'"
+            :class="showMenu ? 'flex overflow-y-auto' : 'hidden'"
             class="absolute top-14 2xsm:top-28 left-0 bg-night h-screen w-full justify-center"
         >
             <span
@@ -424,6 +424,170 @@ export default {
                     <li class="move-right-on-hover">
                         <a href="/endurance-racing">Endurance Racing</a>
                     </li>
+                    <li class="2xsm:hidden pt-4">
+                        <ul class="flex place-items-center">
+                            <a
+                                class="pr-2"
+                                href="https://www.instagram.com/"
+                                target="_blank"
+                                title="Racestop on Instagram"
+                            >
+                                <li>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        viewBox="0,0,256,256"
+                                        width="16px"
+                                        height="16px"
+                                        fill-rule="nonzero"
+                                    >
+                                        <g
+                                            fill="#ffffff"
+                                            fill-rule="nonzero"
+                                            stroke="none"
+                                            stroke-width="1"
+                                            stroke-linecap="butt"
+                                            stroke-linejoin="miter"
+                                            stroke-miterlimit="10"
+                                            stroke-dasharray=""
+                                            stroke-dashoffset="0"
+                                            font-family="none"
+                                            font-weight="none"
+                                            font-size="none"
+                                            text-anchor="none"
+                                            style="mix-blend-mode: normal"
+                                        >
+                                            <g
+                                                transform="scale(9.84615,9.84615)"
+                                            >
+                                                <path
+                                                    d="M7.54688,0c-4.15625,0 -7.54687,3.39063 -7.54687,7.54688v10.90625c0,4.15625 3.39063,7.54688 7.54688,7.54688h10.90625c4.15625,0 7.54688,-3.39062 7.54688,-7.54687v-10.90625c0,-4.15625 -3.39062,-7.54687 -7.54687,-7.54687zM7.54688,2h10.90625c3.07422,0 5.54688,2.46875 5.54688,5.54688v10.90625c0,3.07422 -2.46875,5.54688 -5.54687,5.54688h-10.90625c-3.07422,0 -5.54687,-2.46875 -5.54687,-5.54687v-10.90625c0,-3.07422 2.46875,-5.54687 5.54688,-5.54687zM20.5,4c-0.82812,0 -1.5,0.67188 -1.5,1.5c0,0.82813 0.67188,1.5 1.5,1.5c0.82813,0 1.5,-0.67187 1.5,-1.5c0,-0.82812 -0.67187,-1.5 -1.5,-1.5zM13,6c-3.85547,0 -7,3.14453 -7,7c0,3.85547 3.14453,7 7,7c3.85547,0 7,-3.14453 7,-7c0,-3.85547 -3.14453,-7 -7,-7zM13,8c2.77344,0 5,2.22656 5,5c0,2.77344 -2.22656,5 -5,5c-2.77344,0 -5,-2.22656 -5,-5c0,-2.77344 2.22656,-5 5,-5z"
+                                                />
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </li>
+                            </a>
+                            <a
+                                class="pr-2"
+                                href="https://www.facebook.com/"
+                                target="_blank"
+                                title="Racestop on Facebook"
+                            >
+                                <li>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        viewBox="0,0,256,256"
+                                        width="16px"
+                                        height="16px"
+                                        fill-rule="nonzero"
+                                    >
+                                        <g
+                                            fill="#ffffff"
+                                            fill-rule="nonzero"
+                                            stroke="none"
+                                            stroke-width="1"
+                                            stroke-linecap="butt"
+                                            stroke-linejoin="miter"
+                                            stroke-miterlimit="10"
+                                            stroke-dasharray=""
+                                            stroke-dashoffset="0"
+                                            font-family="none"
+                                            font-weight="none"
+                                            font-size="none"
+                                            text-anchor="none"
+                                            style="mix-blend-mode: normal"
+                                        >
+                                            <g
+                                                transform="scale(10.66667,10.66667)"
+                                            >
+                                                <path
+                                                    d="M12,2c-5.523,0 -10,4.477 -10,10c0,5.013 3.693,9.153 8.505,9.876v-7.226h-2.474v-2.629h2.474v-1.749c0,-2.896 1.411,-4.167 3.818,-4.167c1.153,0 1.762,0.085 2.051,0.124v2.294h-1.642c-1.022,0 -1.379,0.969 -1.379,2.061v1.437h2.995l-0.406,2.629h-2.588v7.247c4.881,-0.661 8.646,-4.835 8.646,-9.897c0,-5.523 -4.477,-10 -10,-10z"
+                                                />
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </li>
+                            </a>
+                            <a
+                                class="pr-2"
+                                href="https://www.twitter.com/"
+                                target="_blank"
+                                title="Racestop on Twitter"
+                            >
+                                <li>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        viewBox="0,0,256,256"
+                                        width="16px"
+                                        height="16px"
+                                        fill-rule="nonzero"
+                                    >
+                                        <g
+                                            fill="#ffffff"
+                                            fill-rule="nonzero"
+                                            stroke="none"
+                                            stroke-width="1"
+                                            stroke-linecap="butt"
+                                            stroke-linejoin="miter"
+                                            stroke-miterlimit="10"
+                                            stroke-dasharray=""
+                                            stroke-dashoffset="0"
+                                            font-family="none"
+                                            font-weight="none"
+                                            font-size="none"
+                                            text-anchor="none"
+                                            style="mix-blend-mode: normal"
+                                        >
+                                            <g
+                                                transform="scale(10.66667,10.66667)"
+                                            >
+                                                <path
+                                                    d="M22,3.999c-0.78,0.463 -2.345,1.094 -3.265,1.276c-0.027,0.007 -0.049,0.016 -0.075,0.023c-0.813,-0.802 -1.927,-1.299 -3.16,-1.299c-2.485,0 -4.5,2.015 -4.5,4.5c0,0.131 -0.011,0.372 0,0.5c-3.353,0 -5.905,-1.756 -7.735,-4c-0.199,0.5 -0.286,1.29 -0.286,2.032c0,1.401 1.095,2.777 2.8,3.63c-0.314,0.081 -0.66,0.139 -1.02,0.139c-0.581,0 -1.196,-0.153 -1.759,-0.617c0,0.017 0,0.033 0,0.051c0,1.958 2.078,3.291 3.926,3.662c-0.375,0.221 -1.131,0.243 -1.5,0.243c-0.26,0 -1.18,-0.119 -1.426,-0.165c0.514,1.605 2.368,2.507 4.135,2.539c-1.382,1.084 -2.341,1.486 -5.171,1.486h-0.964c1.788,1.146 4.065,2.001 6.347,2.001c7.43,0 11.653,-5.663 11.653,-11.001c0,-0.086 -0.002,-0.266 -0.005,-0.447c0,-0.018 0.005,-0.035 0.005,-0.053c0,-0.027 -0.008,-0.053 -0.008,-0.08c-0.003,-0.136 -0.006,-0.263 -0.009,-0.329c0.79,-0.57 1.475,-1.281 2.017,-2.091c-0.725,0.322 -1.503,0.538 -2.32,0.636c0.834,-0.5 2.019,-1.692 2.32,-2.636z"
+                                                />
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </li>
+                            </a>
+                            <a
+                                href="https://www.tiktok.com/"
+                                target="_blank"
+                                title="Racestop on Tiktok"
+                            >
+                                <li>
+                                    <svg
+                                        fill="#ffffff"
+                                        viewBox="0 0 32 32"
+                                        version="1.1"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        stroke="#ffffff"
+                                        width="16px"
+                                        height="16px"
+                                    >
+                                        <g
+                                            id="SVGRepo_bgCarrier"
+                                            stroke-width="0"
+                                        ></g>
+                                        <g
+                                            id="SVGRepo_tracerCarrier"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                        ></g>
+                                        <g id="SVGRepo_iconCarrier">
+                                            <title>tiktok</title>
+                                            <path
+                                                d="M16.656 1.029c1.637-0.025 3.262-0.012 4.886-0.025 0.054 2.031 0.878 3.859 2.189 5.213l-0.002-0.002c1.411 1.271 3.247 2.095 5.271 2.235l0.028 0.002v5.036c-1.912-0.048-3.71-0.489-5.331-1.247l0.082 0.034c-0.784-0.377-1.447-0.764-2.077-1.196l0.052 0.034c-0.012 3.649 0.012 7.298-0.025 10.934-0.103 1.853-0.719 3.543-1.707 4.954l0.020-0.031c-1.652 2.366-4.328 3.919-7.371 4.011l-0.014 0c-0.123 0.006-0.268 0.009-0.414 0.009-1.73 0-3.347-0.482-4.725-1.319l0.040 0.023c-2.508-1.509-4.238-4.091-4.558-7.094l-0.004-0.041c-0.025-0.625-0.037-1.25-0.012-1.862 0.49-4.779 4.494-8.476 9.361-8.476 0.547 0 1.083 0.047 1.604 0.136l-0.056-0.008c0.025 1.849-0.050 3.699-0.050 5.548-0.423-0.153-0.911-0.242-1.42-0.242-1.868 0-3.457 1.194-4.045 2.861l-0.009 0.030c-0.133 0.427-0.21 0.918-0.21 1.426 0 0.206 0.013 0.41 0.037 0.61l-0.002-0.024c0.332 2.046 2.086 3.59 4.201 3.59 0.061 0 0.121-0.001 0.181-0.004l-0.009 0c1.463-0.044 2.733-0.831 3.451-1.994l0.010-0.018c0.267-0.372 0.45-0.822 0.511-1.311l0.001-0.014c0.125-2.237 0.075-4.461 0.087-6.698 0.012-5.036-0.012-10.060 0.025-15.083z"
+                                            ></path>
+                                        </g>
+                                    </svg>
+                                </li>
+                            </a>
+                        </ul>
+                    </li>
+                    <li class="max-2xsm:h-16 2xsm:h-32"></li>
                 </ul>
                 <div class="max-lg:hidden">
                     <h2 class="text-sm">WELCOME TO</h2>
