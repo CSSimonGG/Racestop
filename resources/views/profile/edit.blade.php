1b8044e40eb7b5bc13d96a916a32d9429ac34577
Simon <98284824+CSSimonGG@users.noreply.github.com>
2023-10-05 09:29:41 +0200

    Create post button on profile page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -10,6 +10,17 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @if (Auth::check())
+            @role('writer')
+                <div class="bg-night h-20 flex justify-center">
+                    <div class="mt-6">
+                        <a href="/create" class="bg-light-red uppercase text-white text-sm font-bold py-3 px-5 rounded">
+                            Create post
+                        </a>
+                    </div>
+                </div>
+            @endrole
+        @endif
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     <form method="POST" action="/logout">
