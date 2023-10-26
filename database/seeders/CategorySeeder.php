1e2b86abe13467f6cfe9de28c362ee6e33ea8cf2
Simon <98284824+CSSimonGG@users.noreply.github.com>
2023-10-26 10:22:53 +0200

    Categories factory and seeder renamed to category factory and seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CategoriesSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
