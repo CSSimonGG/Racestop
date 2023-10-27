c5ae690b7a38ebe56df491657fb113381ecb22f4
Simon <98284824+CSSimonGG@users.noreply.github.com>
2023-10-27 09:34:34 +0200

    F1race model, factory, seeder and migration created

---
+++ app/Http/Controllers/F1raceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\F1race;
+use Illuminate\Http\Request;
+
+class F1raceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(F1race $f1race)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(F1race $f1race)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, F1race $f1race)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(F1race $f1race)
+    {
+        //
+    }
+}
