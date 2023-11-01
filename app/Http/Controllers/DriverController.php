9d102afd680287b833437c20e8af36835c1c9ef2
Simon <satmdeklerk@gmail.com>
2023-11-01 18:54:44 +0100

    Driver model, controller, migration, factory and seeder

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\driver;
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(driver $driver)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(driver $driver)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, driver $driver)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(driver $driver)
+    {
+        //
+    }
+}
